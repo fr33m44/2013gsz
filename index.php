6edbbcf74b679ea4a12534f19900654bb51bb4c4
sh1t h4pp3ns <228787+fr33m44@users.noreply.github.com>
2014-08-20 07:55:47 +0000

    删除不再需要的文件。完全支持markdown。

--- index.php
+++ index.php
@@ -5,17 +5,20 @@
 	session_start();
 	if($_SESSION['userid'] == "")
 		header('location:login.php');
+	
+	isset($_GET['a']) ? $a = $_GET['a'] : $a = null;
 	//new edit del data
 	if(isset($_POST['a']))
 	{
-	
-		$a  	= $_POST['a'];
-		if($a == 'edit'){
+		$a 	= $_POST['a'];
+		if($a == 'edit')
+		{
 			$id 	= $_POST['id'];
 			$title  = $_POST['title'];
 			$date 	= $_POST['date'];
-			$content= $_POST['content'];
-			$result = mysql_query($q = sprintf("update diary set date='%s', title='%s', content='%s' where id=%s",$date, $title, $content, $id));
+			$content_text = $_POST['content_text'];
+			$content_html = $_POST['content_html'];
+			$result = mysql_query($q = sprintf("update diary set date='%s', title='%s', content_text='%s', content_html='%s' where id=%s",$date, $title, $content_text, $content_html, $id));
 			
 			if($result)
 			{
@@ -26,8 +29,22 @@
 				echo '保存失败';
 			}
 		}
-		elseif($a == 'new'){
+		elseif($a == 'new')
+		{
+			$title  = $_POST['title'];
+			$date 	= $_POST['date'];
+			$content_text = $_POST['content_text'];
+			$content_html = $_POST['content_html'];
+			$result = mysql_query($q = sprintf("insert into diary set date='%s', title='%s', content_text='%s', content_html='%s' ",$date, $title, $content_text, $content_html));
 			
+			if($result)
+			{
+				echo '保存成功';
+			}
+			else
+			{
+				echo '保存失败';
+			}
 		}
 		elseif($a =='search'){
 			$page_size = 10;
@@ -124,14 +141,17 @@
 		<!--operation-->
 		<div class="grid_6">&nbsp;</div>
 		
-		<div class="grid_3"><input type="text" id="search_key" placeholder="Search Diary Here..." size="20" /><input type="button" id="link_search" value="Search" /></div>
+		<div class="grid_3"><input type="text" id="search_key" placeholder="输入关键字搜索..." size="20" /><input type="button" id="link_search" value="Search" /></div>
 		
-		<div class="grid_1"><a class="linkbtn" href="new.php" id="link_new">New</a></div>
+		<div class="grid_3">
+			<a class="linkbtn" href="index.php?a=new" id="link_new">New</a>
 <?php if($y && $m && $d){?>
-		<div class="grid_1"><a class="linkbtn" href="javascript:;" id="link_edit">Edit</a></div>
-		
-		<div class="grid_1"><a class="linkbtn hide" href="javascript:;" id="link_save">Save</a></div>
+			<a class="linkbtn" href="javascript:;" id="link_edit">Edit</a>
+			<a class="linkbtn" href="javascript:;" id="link_save">Save</a>
+<?php } elseif($a == 'new'){?>
+			<a class="linkbtn" href="javascript:;" id="link_save">Save</a>
 <?php } ?>
+		</div>
 		<div class="clear"></div>
 		<!--list-->
 		<div class="grid_1" id="year">
@@ -208,7 +228,8 @@
 			?>
 			</ul>
 		</div>
-<?php 
+<?php
+//show diary content or show new diary editor UI
 if($y && $m && $d)
 	{
 		$content = mysql_fetch_assoc($content);
@@ -216,13 +237,23 @@ if($y && $m && $d)
 		<div class="grid_7" id="content" name="content">
 			<div id="title"><h2 id="titleHead"><?php echo $content['title']?></h2></div>
 			<div id="dates"><em>Date:<span id="date"><?php echo $content['date']?></span> | Post Date:<?php echo $content['post_date']?> | Last Modified:<?php echo $content['post_modified']?></em></div>
-			<!--<div id="contentText"><?php echo $content['content']?></div>-->
-			<div class="wmd-panel">
-            <div id="wmd-button-bar"></div>
-            <textarea class="wmd-input" id="wmd-input"><?php echo $content['content_text']?></textarea>
-        </div>
-        <div id="wmd-preview" class="wmd-panel wmd-preview"></div>
-			<input type="hidden" name="id" value="<?php echo $content['id']?>"/>
+			<div id="editor" class="wmd-panel">
+				<div id="wmd-button-bar"></div>
+				<textarea class="wmd-input content-md" id="wmd-input"><?php echo $content['content_text']?></textarea>
+				<input type="hidden" name="id" value="<?php echo $content['id']?>"/>
+			</div>
+			<div id="wmd-preview" class="wmd-panel wmd-preview"></div>
+		</div>
+<?php }elseif($a == 'new'){?>		
+		<div class="grid_7" id="content" name="content">
+			<div id="title"><h2><input name="title" id="title" type="text" size="80" style="padding:3px 8px" /></h2></div>
+			<div id="dates"><em>Date:<input type="text" name="date" /></em></div>
+			<div id="editor" class="wmd-panel">
+				<div id="wmd-button-bar"></div>
+				<textarea class="wmd-input content-md" id="wmd-input"></textarea>
+				<input type="hidden" name="id" />
+			</div>
+			<div id="wmd-preview" class="wmd-panel wmd-preview"></div>
 		</div>
 <?php }else {?>
 		<div class="grid_7" id="content" name="content">
@@ -231,6 +262,8 @@ if($y && $m && $d)
 
 		<div class="clear"></div>
 		<!--copyright-->
+		<div class="grid_12" id="horizon"><hr /></div>
+		<div class="clear"></div>
 		<div class="grid_12" id="footer">copyright© 2010-  <a href="http://tunps.com">tunpishuang</a>. All rights reserved.</div>
 		<div class="clear"></div>
 	</div>
@@ -241,42 +274,7 @@ $(function(){
 	if($y && $m && $d){
 ?>
 	var id=<?php echo $content['id']?>;
-	
-	$('#link_edit').click(function(){
-		/*
-		if(typeof $('input[id="title"]').attr('id') == 'undefined')
-		{
-			$('#link_save').removeClass('hide');
-			$.getScript('lib/kindeditor/kindeditor-min.js', function(){
-				KindEditor.basePath = 'lib/kindeditor/';
-				KindEditor.create('div[id="contentText"]');
-			});
-			var title = $("#titleHead").html();
-			var date = $("#date").html();
-			$("#title").html('<h2><input name="title" id="title" type="text" size="80" value="'+title+'"+ style="padding:3px 8px" /></h2>');
-			$("#date").html('<input type="text" name="date" value="'+date+'"/>');
-		}
-		*/
-		if(typeof $('input[id="title"]').attr('id') == 'undefined')
-		{
-			var title = $("#titleHead").html();
-			var date = $("#date").html();
-			$("#title").html('<h2><input name="title" id="title" type="text" size="80" value="'+title+'"+ style="padding:3px 8px" /></h2>');
-			$("#date").html('<input type="text" name="date" value="'+date+'"/>');
-				
-			var converter1 = Markdown.getSanitizingConverter();
-			converter1.hooks.chain("preBlockGamut", function (text, rbg) {
-				return text.replace(/^ {0,3}""" *\n((?:.*?\n)+?) {0,3}""" *$/gm, function (whole, inner) {
-					return "<blockquote>" + rbg(inner) + "</blockquote>\n";
-				});
-			});
-			var editor1 = new Markdown.Editor(converter1);
-			editor1.run();
-		}
-	}); 
-	
 	$('#link_save').click(function(){
-		KindEditor.remove('div[id="wmd-preview"]');
 		$.ajax({
 			type:"POST",
 			contentType: "application/x-www-form-urlencoded;charset=utf-8",
@@ -286,7 +284,8 @@ $(function(){
 					'id':id,
 					'date':$('input[name="date"]').val(),
 					'title':$('input[name="title"]').val(), 
-					'content':$('#contentText').html()
+					'content_text':$('.content-md').val(),
+					'content_html':$('#wmd-preview').html()
 				},
 			success: function(msg){
 				alert(msg);
@@ -294,7 +293,67 @@ $(function(){
 			}
 		}); 
 	});
+	$('#editor').hide();//隐藏编辑界面
+	$('#wmd-preview').show();//显示html
 <?php } ?>
+
+<?php 
+	if($a == 'new'){
+?>
+	$('#link_save').click(function(){
+		$.ajax({
+			type:"POST",
+			contentType: "application/x-www-form-urlencoded;charset=utf-8",
+			url:location.href,
+			data:{
+					'a':'new',
+					'date':$('input[name="date"]').val(),
+					'title':$('input[name="title"]').val(), 
+					'content_text':$('.content-md').val(),
+					'content_html':$('#wmd-preview').html()
+				},
+			success: function(msg){
+				alert(msg);
+				location.replace(location.href);
+			}
+		}); 
+	});
+	$('#editor').hide();//隐藏编辑界面
+	$('#wmd-preview').show();//显示html
+<?php } ?>
+
+<?php 
+	if(($y && $m && $d)|| ($a == 'new')) {
+?>
+	var converter1 = Markdown.getSanitizingConverter();
+			converter1.hooks.chain("preBlockGamut", function (text, rbg) {
+				return text.replace(/^ {0,3}""" *\n((?:.*?\n)+?) {0,3}""" *$/gm, function (whole, inner) {
+					return "<blockquote>" + rbg(inner) + "</blockquote>\n";
+				});
+			});
+	var editor1 = new Markdown.Editor(converter1);
+	editor1.run();
+<?php } ?>
+
+<?php 
+	if($a == 'new') {
+?>
+	$('#editor').show();//隐藏编辑界面
+	$('#wmd-preview').show();//显示html
+<?php } ?>
+
+	$('#link_edit').click(function(){
+		if(typeof $('input[id="title"]').attr('id') == 'undefined')
+		{
+			var title = $("#titleHead").html();
+			var date = $("#date").html();
+			$("#title").html('<h2><input name="title" id="title" type="text" size="80" value="'+title+'"+ style="padding:3px 8px" /></h2>');
+			$("#date").html('<input type="text" name="date" value="'+date+'"/>');
+			$('#editor').show();//隐藏编辑界面
+		}
+	}); 
+	
+
 	
 	$('#link_search').click(function(){
 		$.ajax({
